7d1030d09ef6f97d3f7b0fd7ca0618ddba90f1fa
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-09-23 21:57:45 -0400

    Added export to csv and steps between angles

--- anglefinder.py
+++ anglefinder.py
@@ -1,15 +1,14 @@
 import math
-
+import csv
 
 # DESMOS https://www.desmos.com/calculator/zss45xap3f
 
-# dummy points
-points = [ [29.07,-5.394]]
+stepNum = 20 # number of steps between points
+points = [ [20, 10], [29.07,-5.394], [30, 20] ] # list of x,y positions along desired path
 
 a1 = 22.0 # length of arm 1 
 a2 = 14.0 # length of arm 2 (inches)
 y_offset = 8.0 # a little vertical gap above the joint (inches)
-
 
 # 1 if positive/0, -1 if negative
 def sign(x):
@@ -24,7 +23,6 @@ def findAngles(x,y):
     y -= y_offset
     
     # in radians, angles q1 and q2
-    print((x**2 + y**2 - a1**2 - a2**2) / (2*a1*a2))
     q2 = math.acos((x**2 + y**2 - a1**2 - a2**2) / (2*a1*a2)) * sign(y) * -1
     q1 = math.atan(y / x) - math.atan( (a2 * math.sin(q2) /  (a1 + a2 * math.cos(q2)) ))
 
@@ -33,15 +31,44 @@ def findAngles(x,y):
 
     return [q1,q1+q2] # The q2 angle calculated is relative to the x axis, so we must add q1 to have it relative to the joint
 
+def getPath(p1, p2):
+    x_incr = (p2[0] - p1[0]) / stepNum
+    y_incr = (p2[1] - p1[1]) / stepNum
+    path_points = []
+    for i in range(stepNum):
+        path_points.append([p1[0] + ((i+1) * x_incr), p1[1] + ((i+1) * y_incr)])
+    return path_points
+
+def getFullPath(points):
+    first = points.pop(0)
+    first_point = findAngles(first[0], first[1])
+    full_path = [first_point]
+    for x,y in points:
+        q1,q2 = findAngles(x,y)
+        short_path = getPath(full_path[-1], [q1, q2])
+        full_path += short_path
+    return full_path
 
 def display(points):
 
     print("Converting between given (x,y) points to degree values of Q1 and Q2, indicating the angles of the two joints.")
 
-    for x,y in points:
-        q1,q2 = findAngles(x,y)
-        print("             (x,y)\t\t\tQ1\t\t    Q2")
-        print("({},{})  \t {} \t {}".format(x,y,q1,q2))
+    print_array = [["(x,y)", "Q1", "Q2"]]
+    for x, y in points:
+        q1, q2 = findAngles(x, y)
+        print_array.append(["("+str(x)+","+str(y)+")", str(q1), str(q2)])
 
+    for p in print_array:
+        print("{: >20} {: >20} {: >20}".format(*p))
+
+def export(points):
+    # have array of angle pairs, being the angle for the left then right joints, respectively
+    with open('motion_profile.csv', 'w', newline='') as csvfile:
+        pathwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        full_path = getFullPath(points)
+        for x, y in full_path:
+            pathwriter.writerow([x, y])
 
 display(points)
+export(points)
